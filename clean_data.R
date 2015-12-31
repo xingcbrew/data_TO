7903b790dae4b8f48ef64648bae7f26b344b98d5
xingcbrew <xingcbrew@gmail.com>
2015-12-30 21:59:48 -0500

    more shit done on R script

--- clean_data.R
+++ clean_data.R
@@ -67,8 +67,8 @@ dat$cat_vis_min <- ifelse(dat$per_vis_min > 63.50, "high",
                                    dat$per_vis_min <= 63.5, "medium", "low"))
 
 ## make variable for assaults per capita in each neighbourhood
-dat$assaults_per_cap <- (dat$assaults/dat$total_population)*100
-dat$assaults_per_cap <- round(dat$assaults_per_cap, 2)
+dat$assaults_per_cap <- (dat$assaults/dat$total_population)
+dat$assaults_per_cap <- round(dat$assaults_per_cap, 4)
 
 ## explore average income
 summary(dat$average_family_income)
@@ -79,3 +79,33 @@ dat$cat_avg_income <- ifelse(dat$average_family_income > 92800, "high",
                              ifelse(dat$average_family_income > 65840 &
                                       dat$average_family_income <= 92800,"medium",
                                     "low"))
+
+
+## see if there is pattern between percentage visible minority and average income
+library(ggplot2)
+library(scales)
+
+ggplot(dat, aes(x=dat$per_vis_min, y=dat$average_family_income)) +
+  geom_point(shape = 1, alpha=1/4) +
+  scale_colour_hue(l=50) +
+  geom_smooth(method=lm, color = "orange") +
+  xlab("Percentage Visible Minority") +
+  ylab("Average Family Income") +
+  ggtitle("Percentage of Visible Minorities vs Family Income by Toronto Neighbourhoods") +
+  scale_y_continuous(labels = comma)
+
+## determine which neighbourhoods have highest assaults per capita
+dat$neighbourhood[dat$assaults_per_cap == max(dat$assaults_per_cap)]
+# Highest is Bay Street Corridor
+
+## determine unemployment rate by neighbourhood
+dat$per_unemployed <- (dat$unemployed/dat$total_population)*100
+dat$per_unemployed <- round(dat$per_unemployed, 2)
+
+## explore unemployment rates
+summary(dat$per_unemployed)
+dat$neighbourhood[dat$per_unemployed == max(dat$per_unemployed)]
+dat$neighbourhood[dat$per_unemployed < 3]
+
+
+
